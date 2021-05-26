a6dfa5fb496a7348628cd9d25a0ca55b01d13e5c
Quin Kennedy <quinkennedy@gmail.com>
2021-05-25 19:46:05 -0700

    using neopixel wing

--- code/proto_qtpi/proto_qtpi.ino
+++ code/proto_qtpi/proto_qtpi.ino
@@ -1,6 +1,14 @@
+
+//#define USE_BAR
+#define USE_GRID
+//#define RANDOMIZE_GRID
+#define SNAKE_GRID
+
 #include <Adafruit_GFX.h>
+#ifdef USE_BAR
 #include <Adafruit_LEDBackpack.h>
-#include "SparkFun_Qwiic_Twist_Arduino_Library.h"
+#endif
+//#include "SparkFun_Qwiic_Twist_Arduino_Library.h"
 #include <Bounce2.h>
 #include <Adafruit_NeoPixel.h>
 #include <SimpleRotary.h>
@@ -21,15 +29,30 @@
 #define IN_LED 21
 #define REC_LED 23
 #define OUT_LED 0
+#ifdef USE_BAR
 #define QUEUE_OFFSET 2
 #define QUEUE_LENGTH 8
+#endif
+#ifdef USE_GRID
+#define QUEUE_OFFSET 0
+#define QUEUE_LENGTH 32
+#endif
 #define BTN_REC 1
 #define BTN_IN 0
 #define OUT_TRIGGER 9
+#define NEO_PIN 4
+#define LED_BRIGHTNESS 4
 
-Adafruit_NeoPixel pixels(1, PIN_NEOPIXEL);
+Adafruit_NeoPixel pixio(1, PIN_NEOPIXEL);
+Adafruit_NeoPixel grid(32, NEO_PIN);
+uint32_t activeColor = Adafruit_NeoPixel::Color(LED_BRIGHTNESS, LED_BRIGHTNESS, 0);
+uint32_t inactiveColor = Adafruit_NeoPixel::Color(LED_BRIGHTNESS/4, LED_BRIGHTNESS/4, 0);
+uint32_t triggeredColor = Adafruit_NeoPixel::Color(LED_BRIGHTNESS*8, LED_BRIGHTNESS*8, 0);
+uint32_t offColor = 0;
+#ifdef USE_BAR
 Adafruit_24bargraph bar = Adafruit_24bargraph();
-TWIST twist;
+#endif
+//TWIST twist;
 // Pin A, Pin B, Button Pin
 SimpleRotary rotary(3, 2, 8);
 
@@ -59,21 +82,46 @@ unsigned long prevSignalRise = 0;
 bool trigHigh = false;
 bool sigHigh = false;
 uint16_t sinceRise = 0;
+#ifdef RANDOMIZE_GRID
+uint8_t r[] = {
+  0, 13, 8, 27, 23, 9, 20, 7, 
+  31, 5, 22, 18, 21, 16, 1, 3, 
+  19, 2, 24, 29, 15, 17, 28, 11, 
+  6, 12, 26, 14, 4, 30, 10, 25
+};
+#elif defined SNAKE_GRID
+uint8_t r[] = {
+  7, 15, 23, 31, 30, 22, 14, 6,
+  5, 13, 21, 29, 28, 20, 12, 4,
+  3, 11, 19, 27, 26, 18, 10, 2,
+  1, 9,  17, 25, 24, 16, 8,  0 
+};
+#else
+uint8_t r[] = {
+  7, 15, 23, 31, 6, 14, 22, 30,
+  5, 13, 21, 29, 4, 12, 20, 28,
+  3, 11, 19, 27, 2, 10, 18, 26,
+  1, 9,  17, 25, 0, 8,  16, 24
+};
+#endif
 
 void setup(){
   //init inputs
   pinMode(IN_SIGNAL, INPUT_PULLUP);
   pinMode(BTN_IN, INPUT_PULLUP);
   pinMode(BTN_REC, INPUT_PULLUP);
-  twist.begin();
+  //twist.begin();
 
   //init outputs
   pinMode(OUT_TRIGGER, OUTPUT);
   //pinMode(IN_LED, OUTPUT);
-  pixels.begin();
-  bar.begin(0x70);  // pass in the address
-  bar.clear();
-  bar.writeDisplay();
+  pixio.begin();
+  #ifdef USE_BAR
+  initBar();
+  #endif
+  #ifdef USE_GRID
+  initGrid();
+  #endif
 
   //set outputs low
   digitalWrite(OUT_TRIGGER, LOW);
@@ -87,6 +135,32 @@ void setup(){
   recB.attach(BTN_REC);
   recB.interval(debounceDurationMs);
 
+}
+
+#ifdef USE_GRID
+void initGrid(){
+  grid.begin();
+  grid.clear();
+  for(int j = 1; j <= 3; j++){
+  for(int i = 7; i >= 0; i--){
+    for(int k = 0; k < 4; k++){
+      grid.setPixelColor(k*8+i, grid.Color((j&1)*LED_BRIGHTNESS, (j&2)*LED_BRIGHTNESS/2, 0));
+    }
+    grid.show();
+    delay(40);
+  }
+  }
+  grid.clear();
+  grid.show();
+}
+#endif
+
+#ifdef USE_BAR
+void initBar(){
+  bar.begin(0x70);  // pass in the address
+  bar.clear();
+  bar.writeDisplay();
+  
 //a fun startup animation
   bar.setBrightness(1);
   for(int j = 1; j <= 3; j++){
@@ -103,6 +177,7 @@ void setup(){
   bar.writeDisplay();
   bar.setBrightness(15);
 }
+#endif
 
 void loop(){
   
@@ -113,8 +188,13 @@ void loop(){
   if (sigHigh && sinceRise > triggerDurationMs){
     sigHigh = false;
     //digitalWrite(IN_LED, LOW);
+    #ifdef USE_BAR
     bar.setBar(IN_LED, LED_OFF);
     bar.writeDisplay();
+    #else
+    pixio.setPixelColor(0, pixio.Color(0, 0, 0));
+    pixio.show();
+    #endif
   }
 
   //if the next event is scheduled to happen now
@@ -122,6 +202,10 @@ void loop(){
     if (curr->index < numActive){
   
       startTrigger();
+      #ifdef USE_GRID
+      grid.setPixelColor(r[curr->index], triggeredColor);
+      grid.show();
+      #endif
     }
 
     prev = curr;
@@ -130,8 +214,19 @@ void loop(){
     if (trigHigh && sinceRise >= trigLowTime){
       trigHigh = false;
       digitalWrite(OUT_TRIGGER, LOW);
+      #ifdef USE_BAR
       bar.setBar(OUT_LED, LED_OFF);
       bar.writeDisplay();
+      #else
+      pixio.setPixelColor(0, pixio.Color(0, 0, 0));
+      pixio.show();
+      #endif
+      #ifdef USE_GRID
+      //TODO: there is probably a bug here around multiple triggers too close
+      // or "deactivating" a trigger as it plays
+      grid.setPixelColor(r[prev->index], activeColor);
+      grid.show();
+      #endif
     }
   }
 }
@@ -141,47 +236,87 @@ inline void startTrigger(){
     trigHigh = true;
     digitalWrite(OUT_TRIGGER, HIGH);
     trigLowTime = sinceRise + triggerDurationMs;
+    #ifdef USE_BAR
+    bar.setBar(OUT_LED, LED_GREEN);
+    bar.writeDisplay();
+    #else
+    pixio.setPixelColor(0, pixio.Color(0, 0, 20));
+    pixio.show();
+    #endif
   }
 }
 
 inline void handleInputs(){
-  byte i = rotary.rotate();
-  if (i != 0){
+  byte b = rotary.push();
+  if (b == 1){
+    clearEvents();
+  }
+  byte rot = rotary.rotate();
+  if (rot != 0){
     bool changed = false;
-    if (i == 1 && numActive < numNodes){
+    if (rot == 1 && numActive < numNodes){
       numActive++;
       changed = true;
     }
-    if (i == 2 && numActive > 0){
+    if (rot == 2 && numActive > 0){
       numActive--;
       changed = true;
     }
     if (changed){
-      if (i == 1 && numActive <= QUEUE_LENGTH){
+      if (rot == 1 && numActive <= QUEUE_LENGTH){
+        #ifdef USE_BAR
         bar.setBar(QUEUE_OFFSET + numActive - 1, LED_GREEN);
         bar.writeDisplay();
+        #endif
+        #ifdef USE_GRID
+        grid.setPixelColor(r[QUEUE_OFFSET + numActive - 1], activeColor);
+        grid.show();
+        #endif
       }
-      if (i == 2 && numActive < QUEUE_LENGTH){
+      if (rot == 2 && numActive < QUEUE_LENGTH){
+        #ifdef USE_BAR
         bar.setBar(QUEUE_OFFSET + numActive, LED_RED);
         bar.writeDisplay();
+        #endif
+        #ifdef USE_GRID
+        grid.setPixelColor(r[QUEUE_OFFSET + numActive], inactiveColor);
+        grid.show();
+        #endif
       }
     }
   }
   if (recB.update()){
     if (recB.fell()){
+      #ifdef USE_BAR
       bar.setBar(REC_LED, LED_RED);
+      #else
+      pixio.setPixelColor(0, pixio.Color(20, 0, 0));
+      #endif
     } else {
+      #ifdef USE_BAR
       bar.setBar(REC_LED, LED_OFF);
+      #else
+      pixio.setPixelColor(0, pixio.Color(0, 0, 0));
+      #endif
     }
+    #ifdef USE_BAR
     bar.writeDisplay();
+    #else
+    pixio.show();
+    #endif
   }
   if (sigB.update()){
     if (sigB.fell()){
       restartSequence();
       sigHigh = true;
       //digitalWrite(IN_LED, HIGH);
+      #ifdef USE_BAR
       bar.setBar(IN_LED, LED_GREEN);
       bar.writeDisplay();
+      #else
+      pixio.setPixelColor(0, pixio.Color(0, 20, 0));
+      pixio.show();
+      #endif
     }
   }
   if (inB.update()){
@@ -237,7 +372,12 @@ inline void clearUndos(){
         curr = _curr;
       }
       if (_removed->index < QUEUE_LENGTH){
+        #ifdef USE_BAR
         bar.setBar(_removed->index + QUEUE_OFFSET, LED_OFF);
+        #endif
+        #ifdef USE_GRID
+        grid.setPixelColor(r[_removed->index+QUEUE_OFFSET], offColor);
+        #endif
       }
       delete _removed;
       numNodes--;
@@ -283,8 +423,14 @@ inline void insertEvent(){
 
   //add bar to graph
   if (numNodes < QUEUE_LENGTH){
+    #ifdef USE_BAR
     bar.setBar(numNodes + QUEUE_OFFSET, LED_GREEN);
     bar.writeDisplay();
+    #endif
+    #ifdef USE_GRID
+    grid.setPixelColor(r[numNodes + QUEUE_OFFSET], activeColor);
+    grid.show();
+    #endif
   }
   numNodes++;
   numActive++;
@@ -307,8 +453,14 @@ inline void clearEvents(){
   curr = NULL;
   numNodes = 0;
   numActive = 0;
+  #ifdef USE_BAR
   for(uint8_t i = QUEUE_LENGTH + QUEUE_OFFSET - 1; i >= QUEUE_OFFSET; i--){
     bar.setBar(i, LED_OFF);
   }
   bar.writeDisplay();
+  #endif
+  #ifdef USE_GRID
+  grid.clear();
+  grid.show();
+  #endif
 }
